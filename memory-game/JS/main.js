8776223fad01675d0dee52035593b4bce01f7444
Jon Carlo Vargas <jcjv94@gmail.com>
2019-09-24 15:55:23 -0700

    restructured folder

---
+++ memory-game/JS/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "King";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "Queen";
+console.log("User Flipped" + cardTwo);
+console.log("User Flipped" + cardFour);
